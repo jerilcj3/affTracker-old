4758cb551f2bd8134c62a683def9c9ada5a71ac2
jerilcj3 <jerilcj3@gmail.com>
2022-09-02 22:22:43 +0530

    campaign section completed for backend

--- models/Campaign.js
+++ models/Campaign.js
@@ -1,14 +1,30 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 /* PetSchema will correspond to a collection in your MongoDB database. */
 const CampaignSchema = new mongoose.Schema({
-  name: {
+  campaign_name: {
     /* The name of this pet */
 
     type: String,
-    required: [true, 'Please provide a name for this Campaign.'],
-    maxlength: [60, 'Name cannot be more than 60 characters'],
-  },  
-})
+    required: [true, "Please provide a name for this Campaign."],
+    maxlength: [60, "Name cannot be more than 60 characters"],
+  },
+  created_at: { type: Date, required: [true, "Created Date Required"] },
+  ad_angle: {
+    /* The name of this pet */
 
-export default mongoose.models.Campaign || mongoose.model('Campaign', CampaignSchema)
+    type: String,
+    required: [true, "Please provide an angle for this campaign"],
+    maxlength: [60, "Name cannot be more than 60 characters"],
+  },
+  traffic_source: {
+    /* The name of this pet */
+
+    type: String,
+    required: [true, "Please provide the traffic source"],
+    maxlength: [60, "Name cannot be more than 60 characters"],
+  },
+});
+
+export default mongoose.models.Campaign ||
+  mongoose.model("Campaign", CampaignSchema);
---
+++ pages/api/campaigns.js
@@ -0,0 +1,19 @@
+import dbConnect from "../../config/dbConnect";
+import nc from "next-connect";
+import {showAllCampaigns, createACampaign} from '../../controllers/campaignController'
+
+dbConnect();
+
+const handler = nc({
+  onError: (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).end("Something broke!");
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end("Page is not found");
+  },
+}).get(showAllCampaigns)
+  .post(createACampaign);
+ 
+
+export default handler;
